98dacdbb158b1a8581555967ddb97494484934c5
spverma536 <spverma536@gmail.com>
2020-08-02 19:09:52 +0530

    changed about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <React.Fragment>
             <h1>About</h1>
-            <p>Data is being fetched by JSON placeholder which is a used as a mimic of HTTP request using axios</p>
+            <p>Data is being fetched by JSON placeholder which is a used as a mimic backend as HTTP request using axios</p>
             <p>This is TodoList app, Made by Piyush Verma.</p>
         </React.Fragment>
     )
