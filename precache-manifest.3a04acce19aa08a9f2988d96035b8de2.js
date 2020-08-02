44d404f7fd8d3e264823587d8f92978c6a6140cf
spverma536 <spverma536@gmail.com>
2020-08-02 19:07:19 +0530

    Updates

---
+++ precache-manifest.3a04acce19aa08a9f2988d96035b8de2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd4fc0dd03844adf054627b6f165958",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "e5a08d7baf87b2064019",
+    "url": "/react-to-do/static/css/main.574ce038.chunk.css"
+  },
+  {
+    "revision": "40573a5c40e20df5bbde",
+    "url": "/react-to-do/static/js/2.0d0b9e31.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react-to-do/static/js/2.0d0b9e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a08d7baf87b2064019",
+    "url": "/react-to-do/static/js/main.47dbfa6a.chunk.js"
+  },
+  {
+    "revision": "937a9eca39302de3b5e8",
+    "url": "/react-to-do/static/js/runtime-main.7927655d.js"
+  }
+]);
